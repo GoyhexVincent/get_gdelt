3eb6b703e79b5c3cd1a839dddb98786fe5d5dc4c
Oryx <vgoyhex@gmail.com>
2017-06-13 20:58:32 +0200

    Typo error

--- docker/postgis-db/docker_cmd.sh
+++ docker/postgis-db/docker_cmd.sh
@@ -11,7 +11,7 @@ docker build -t $img_name .
 docker rm -f $cont_name
 docker run --name $cont_name -e POSTGRES_PASSWORD=postgres -d -v  $PWD/../data:/var/lib/postgresql/data $img_name
 
-until docker run --rm --link $cont_name:pg mappy/postgis-data pg_isready -U redpig -h pg; do sleep 5; done
+until docker run --rm --link $cont_name:pg get_gdelt pg_isready -U redpig -h pg; do sleep 5; done
 
 docker run -it --link $cont_name:postgres --rm postgres \
 	    sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U oryx -d gdelt_db'
