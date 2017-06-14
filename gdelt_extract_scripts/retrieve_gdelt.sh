f929bcb794d8fd5bff6a30d563bc05a840270377
Oryx <vgoyhex@gmail.com>
2017-06-14 21:28:46 +0200

    now dl gdelt_data into a specific dir

--- gdelt_extract_scripts/retrieve_gdelt.sh
+++ gdelt_extract_scripts/retrieve_gdelt.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+rm -rf /gdelt_data
+mkdir gdelt_data
+
 date=$(date -d "1 day ago" +%Y%m%d)
 echo  $date
 wget -P ./gdelt_data http://data.gdeltproject.org/events/$date.export.CSV.zip
+
+cd ./gdelt_data
+
