c0b0c4be081a190dcb0528e329cd5fee445ef4b6
vincent <vincent.goyhex@mappy.com>
2017-06-29 17:58:40 +0200

    Added command to create schemas, table and extensions into the database

--- gdelt_extract_scripts/gdelt_cmd.sh
+++ gdelt_extract_scripts/gdelt_cmd.sh
@@ -2,7 +2,11 @@
 container_name='gdelt_db'
 host=$(docker inspect --format="{{ .NetworkSettings.IPAddress }}" $container_name)
 
+psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-docker pull alpine
-docker run --rm  -v $PWD:/mnt/scripts --link $container_name:gdelt_db alpine retrieve_gdelt.sh 
+psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE EXTENSION IF NOT EXISTS hstore;"
+
+psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE SCHEMA gdelt;"
+
+psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE TABLE data (id bigserial PRIMARY KEY NOT NULL, goldsteinscale integer NOT NULL, date_added timestamp default NULL);"
 
