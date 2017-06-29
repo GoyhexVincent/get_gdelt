0c528bceffd4675c60b01a0611baa3f87eb00000
Oryx <vgoyhex@gmail.com>
2017-06-29 23:24:22 +0200

    added a unzip + started the copy psql command

--- gdelt_extract_scripts/retrieve_gdelt.sh
+++ gdelt_extract_scripts/retrieve_gdelt.sh
@@ -9,9 +9,11 @@ wget -P ./gdelt_data http://data.gdeltproject.org/events/$date.export.CSV.zip
 
 cd ./gdelt_data
 
+unzip $date.export.CSV.zip
+rm *.zip
 #bin/bash
 container_name='gdelt_db'
 host=$(docker inspect --format="{{ .NetworkSettings.IPAddress }}" $container_name)
 
-psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE EXTENSION hstore;"
+psql -h $host -d gdelt_db -p 5432 -U oryx -c "COPY gdelt.data FROM '$date.export.CSV' WITH DELIMITER AS  E'\t'"
 
