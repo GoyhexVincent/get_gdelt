c4201a4e0c43953d6d3821d62260d0c4c81e548b
vincent <vincent.goyhex@mappy.com>
2017-07-07 17:51:42 +0200

    added a proper create table column list

--- gdelt_extract_scripts/gdelt_cmd.sh
+++ gdelt_extract_scripts/gdelt_cmd.sh
@@ -8,5 +8,5 @@ psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE EXTENSION IF NOT EXISTS hst
 
 psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE SCHEMA gdelt;"
 
-psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE TABLE gdelt.data (id bigserial PRIMARY KEY NOT NULL, goldsteinscale integer NOT NULL, date_added timestamp default NULL);"
+psql -h $host -d gdelt_db -p 5432 -U oryx -c "CREATE TABLE gdelt.data (GlobalEventID bigserial PRIMARY KEY NOT NULL, Day INTEGER, MonthYear INTEGER, Year INTEGER, FractionDate INTEGER, Actor1Code VARCHAR, Actor1Name VARCHAR, Actor1CountryCode VARCHAR, Actor1KnownGroupCode VARCHAR, Actor1EthnicCode VARCHAR, Actor1Religion1Code VARCHAR, Actor1Religion2Code varchar, Actor1Type1Code varchar, Actor1Type2Code VARCHAR, Actor1Type3Code VARCHAR, IsRootEvent VARCHAR, EventCode INTEGER, EventBaseCode INTEGER, EventRootCode INTEGER, QuadClass INTEGER, GoldsteinScale INTEGER, NumMentions INTEGER, NumSources INTEGER, NumArticles INTEGER, AvgTone INTEGER, Actor1Geo_Type INTEGER, Actor1Geo_Fullname VARCHAR, Actor1Geo_CountryCode VARCHAR, Actor1Geo_ADM1Code VARCHAR, Actor1Geo_Lat FLOAT, Actor1Geo_Long FLOAT, Actor1Geo_FeatureID INTEGER, DATEADDED INTEGER, SOURCEURL VARCHAR);"
 
