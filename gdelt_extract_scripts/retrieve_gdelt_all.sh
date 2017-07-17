9ac66c7e8c5f430ba6ffadefda3d919eb3a472a5
vincent <vincent.goyhex@mappy.com>
2017-07-17 17:34:22 +0200

    Removed useless README file + started looping over ZIP files in order to populate the db

--- gdelt_extract_scripts/retrieve_gdelt_all.sh
+++ gdelt_extract_scripts/retrieve_gdelt_all.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 echo 'The following wget command allows you to download everything from 20150101 to 20171231'
 echo 'Feel free to change the settings according to your needs: {starting_year..ending_year}{starting_month..ending_month}{starting_day..ending_day}'
-wget -P ./gdelt_data http://data.gdeltproject.org/events/20{15..17}{01..12}{01..31}.export.CSV.zip
+#wget -P ./gdelt_data http://data.gdeltproject.org/events/20{15..17}{01..12}{01..31}.export.CSV.zip
+cd ./gdelt_data
+array=($(ls))
+
+for i in  "${array[@]}"
+do
+	echo "$i"
+	unzip $i
+	rm $i
+done
+
