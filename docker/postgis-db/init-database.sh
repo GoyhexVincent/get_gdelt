1fbb78f82e991273a3723886f896a26adf46e9cf
vincent <vincent.goyhex@mappy.com>
2017-07-19 17:04:05 +0200

    Changed default user:psswd to postgres:postgres

--- docker/postgis-db/init-database.sh
+++ docker/postgis-db/init-database.sh
@@ -11,13 +11,13 @@ then
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE gdelt_db;"
 fi
 
-echo "SELECT 'Ok' AS result FROM pg_roles WHERE rolname='oryx'" | psql --username "$POSTGRES_USER" | grep Ok
+echo "SELECT 'Ok' AS result FROM pg_roles WHERE rolname='postgres'" | psql --username "$POSTGRES_USER" | grep Ok
 if [ $? = 1 ] 
 then
     touch /var/lib/postgresql/data/dockerinfo/user-redpig-created
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        CREATE ROLE oryx LOGIN
-          ENCRYPTED PASSWORD 'crake'
+        CREATE ROLE postgres LOGIN
+          ENCRYPTED PASSWORD 'postgres'
           SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
 EOSQL
 fi
--- gdelt_extract_scripts/retrieve_gdelt_all.sh
+++ gdelt_extract_scripts/retrieve_gdelt_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'The following wget command allows you to download everything from 20150101 to 20171231'
 echo 'Feel free to change the settings according to your needs: {starting_year..ending_year}{starting_month..ending_month}{starting_day..ending_day}'
-#wget -P ./gdelt_data http://data.gdeltproject.org/events/20{15..17}{01..12}{01..31}.export.CSV.zip
+wget -P ./gdelt_data http://data.gdeltproject.org/events/2017{01..12}{01..31}.export.CSV.zip
 
-export PGPASSWORD='crake'
+export PGPASSWORD='postgres'
 
 container_name='gdelt_db'
 host=$(docker inspect --format="{{ .NetworkSettings.IPAddress }}" $container_name)
@@ -17,7 +17,8 @@ do
 	unzip $i
 	rm $i
         CSV="${i::-4}"
-	psql -h $host -d gdelt_db -p 5432 -U oryx -c "\COPY gdelt.data FROM '$CSV' WITH DELIMITER AS  E'\t'"
+	psql -h $host -d gdelt_db -p 5432 -U postgres -c "\COPY gdelt.data FROM '$CSV' WITH DELIMITER AS  E'\t'"
 	echo 'import successful!'
+	rm $CSV
 done
 
