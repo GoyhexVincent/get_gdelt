980db883d1886366417782995354745268bbf131
Oryx <vgoyhex@gmail.com>
2017-07-28 22:41:14 +0200

    added postgresql folder

---
+++ gdelt_extract_scripts/gdelt_postgresql/retrieve_gdelt_db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -rf /gdelt_data
+mkdir gdelt_data
+
+date=$(date -d "1 day ago" +%Y%m%d)
+echo  $date
+wget -P ../gdelt_data http://data.gdeltproject.org/events/$date.export.CSV.zip
+
+cd ./gdelt_data
+
+unzip $date.export.CSV.zip
+rm *.zip
+#bin/bash
+container_name='gdelt_db'
+host=$(docker inspect --format="{{ .NetworkSettings.IPAddress }}" $container_name)
+
+export PGPASSWORD='postgres'
+
+psql -h $host -d gdelt_db -p 5432 -U postgres -c "\COPY gdelt.data FROM '$date.export.CSV' WITH DELIMITER AS  E'\t'"
+#rm $date.export.CSV
+echo 'import successful!'
