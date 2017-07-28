8f77f6ae7cb40e7b97edcdd1f3eda565360fd561
Oryx <vgoyhex@gmail.com>
2017-07-28 22:43:28 +0200

    Variable for docker image clickhouse server

--- docker/clickhouse/docker_cmd.sh
+++ docker/clickhouse/docker_cmd.sh
@@ -2,7 +2,7 @@ echo ' Every commands needed to pull image + create container for clickhouse'
 
 img_name='yandex/clickhouse-server'
 cont_name='gdelt_clickhouse'
-docker pull yandex/clickhouse-server
+docker pull $img_name
 
 docker rm -f $cont_name
 docker run -d --name $cont_name --ulimit nofile=262144:262144 $img_name
