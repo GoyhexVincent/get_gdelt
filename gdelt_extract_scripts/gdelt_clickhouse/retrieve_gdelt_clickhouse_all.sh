548ff4350f510f9ba323b020ab6f99aa2e7f73b6
vincent <vincent.goyhex@mappy.com>
2017-07-31 15:36:23 +0200

    Now taking 3 years worth of data for download

--- gdelt_extract_scripts/gdelt_clickhouse/retrieve_gdelt_clickhouse_all.sh
+++ gdelt_extract_scripts/gdelt_clickhouse/retrieve_gdelt_clickhouse_all.sh
@@ -3,7 +3,7 @@ echo 'The following wget command allows you to download everything from 20150101
 echo 'Feel free to change the settings according to your needs: {starting_year..ending_year}{starting_month..ending_month}{starting_day..ending_day}'
 mkdir gdelt_data
 
-wget -P ./gdelt_data http://data.gdeltproject.org/events/2017{01..02}{01..31}.export.CSV.zip
+wget -P ./gdelt_data http://data.gdeltproject.org/events/20{15..17}{01..12}{01..31}.export.CSV.zip
 
 
 container_name='gdelt_clickhouse'
