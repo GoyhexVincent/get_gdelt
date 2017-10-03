7cde0fe68f3c1b415d127a520a6970d7821579f3
vincent <vincent.goyhex@mappy.com>
2017-10-03 17:45:35 +0200

    Creating a dataset for every horrible stuff that happenned in the past few years + now downloading data down to 2013

--- test_csv.py
+++ test_csv.py
@@ -6,10 +6,10 @@ import csv
 client = Client('172.17.0.2')
 print(client.execute('SHOW TABLES'))
 
-#x = client.execute('SELECT ActionGeo_Lat, ActionGeo_Long FROM gdelt WHERE Actor1Code = \'NGOHLHIRC\' ORDER BY EventCode DESC')
-x = client.execute('SELECT ActionGeo_Lat, ActionGeo_Long, EventCode FROM gdelt WHERE Actor1Code = \'NGOHLHIRC\' and ActionGeo_Lat != \'\' and ActionGeo_Long != \'\'')
+x = client.execute('SELECT GlobalEventID, Actor1CountryCode, Actor1KnownGroupCode, Actor1Religion1Code,  FractionDate, Year, Actor1Geo_CountryCode, IsRootEvent, GoldsteinScale, NumMentions, NumSources, ActionGeo_Lat, ActionGeo_Long, EventCode, SOURCEURL FROM gdelt WHERE EventCode like \'2%\' and ActionGeo_Lat != \'\' and ActionGeo_Long != \'\';')
+
 with open('results.csv', 'wb') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(['lat', 'long', 'EventCode'])
+        filewriter.writerow(['GlobalEventID','Actor1CountryCode', 'Actor1KnownGroupCode,', 'Actor1Religion1Code','FractionDate','Year','Actor1Geo_CountryCode','IsRootEvent','GoldsteinScale','NumMentions', 'NumSources','lat', 'long', 'EventCode','SOURCEURL'])
         for y in x:
-            filewriter.writerow([y[0],y[1],y[2]])
+            filewriter.writerow([y[0],y[1],y[2],y[3],y[4],y[5],y[6],y[7],y[8],y[9],y[10],y[11],y[12],y[13],y[14]])
